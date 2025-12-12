36f0b7a5229837ebfb99154bcf1a5f0b081c7500
Igor <igrfclima@gmail.com>
2025-12-12 19:49:18 -0300

    feat(main) implement all strategies

--- src/br/so/Main.java
+++ src/br/so/Main.java
@@ -1,5 +1,8 @@
 import algorithms.FIFOStrategy;
+import algorithms.LRUStrategy;
+import algorithms.OptimalStrategy;
 import algorithms.PageReplacementStrategy;
+import algorithms.RandomStrategy;
 import core.Simulator;
 import model.SystemConfig;
 import java.util.ArrayList;
@@ -8,35 +11,47 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         try {
+            if (!scanner.hasNextInt()) return;
+
             int physicalMemory = scanner.nextInt();
             int virtualMemory = scanner.nextInt();
             String architecture = scanner.next();
             int totalPages = scanner.nextInt();
-
             SystemConfig config = new SystemConfig(physicalMemory, virtualMemory, architecture, totalPages);
+
+            System.out.println(config.getPageSize());
+            System.out.println(config.getNumFrames());
+            System.out.println(config.getMinSwapSize());
+            System.out.println();
+
             Simulator simulator = new Simulator(config);
+
             int numSequences = scanner.nextInt();
 
             for (int i = 0; i < numSequences; i++) {
                 int sequenceSize = scanner.nextInt();
-                List<Integer> pages = new ArrayList<>();
 
+                List<Integer> pages = new ArrayList<>();
                 for (int j = 0; j < sequenceSize; j++) {
                     pages.add(scanner.nextInt());
                 }
-
                 List<PageReplacementStrategy> strategies = Arrays.asList(
-                    new FIFOStrategy()
+                    new FIFOStrategy(),
+                    new RandomStrategy(),
+                    new LRUStrategy(),
+                    new OptimalStrategy()
                 );
                 simulator.runSimulation(pages, strategies);
+
                 System.out.println();
             }
 
         } catch (Exception e) {
+            System.err.println("Erro durante a execução: " + e.getMessage());
             e.printStackTrace();
         } finally {
             scanner.close();
